f0447e3912c9d794bce025d109376978972bba13
keysson Gomes <keyssoncardoso0@gmail.com>
2026-04-11 19:05:48 -0300

    Corrigindo classe utils, e removendo DymanicURLFilter

--- src/main/java/keysson/apis/empresa/config/SecurityConfig.java
+++ src/main/java/keysson/apis/empresa/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package keysson.apis.empresa.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -16,8 +17,6 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
 
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
@@ -26,8 +25,6 @@ public class SecurityConfig {
     @Autowired
     private JwtAuthenticationFilter jwtAuthenticationFilter;
 
-    @Autowired
-    private DynamicURLFilter dynamicURLFilter;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -46,7 +43,6 @@ public class SecurityConfig {
                 )
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
-                .addFilterAfter(dynamicURLFilter, JwtAuthenticationFilter.class)
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 );
